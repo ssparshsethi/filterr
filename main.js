a97a04b3e31ba051c6999b6600d99c41fa8573d9
ssparshsethi <142225493+ssparshsethi@users.noreply.github.com>
2024-02-05 21:49:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ noseX=0;
 noseY=0;
 
 function preload() {
-clown_nose= loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
-crown= loadimage('https://i.postimg.cc/kG6fzPx5/Coronet-of-the-British-Heir-Apparent-svg.png');
 }
 
 function setup() {
@@ -37,13 +35,9 @@ function draw() {
   image(video, 0, 0, 300, 300);
   fill(255,0,0);
   stroke(255,0,0);
- image(clown_nose, noseX, noseY, 30, 30)
-
+  circle(noseX, noseY, 20);
 }
 
 function take_snapshot(){    
   save('myFilterImage.png');
 }
-function upcoming(){
-  window.location="indexhome.html"
-} 
